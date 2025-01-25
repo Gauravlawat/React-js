cb6664da6151fe25d8b0de4b734a322f36c866ad
Gauravlawat <gauravsoni32443@gmail.com>
2025-01-26 01:15:49 +0530

    build multi components having same structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import reactImg from './assets/react-core-concepts.png';
+// import componentImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data.js';
+const reactDescriptions = ['Fundamental', 'Core', 'Crucial'];
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
 
-const reactDescriptions = ['Fundamental','Core','Crucial'];
-function getRandomInt(max){
-  return Math.floor(Math.random() * (max+1));
+function CoreConcept({image,title,description}) {
+  return (
+    <li>
+      {/* <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p> */}
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
 }
 function Header() {
-  const description=reactDescriptions[getRandomInt(2)];
+  const description = reactDescriptions[getRandomInt(2)];
   return (
     <header>
       <img src={reactImg} alt="Stylized atom" />
@@ -24,7 +38,19 @@ function App() {
       <Header />
 
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
       </main>
     </div>
   );
