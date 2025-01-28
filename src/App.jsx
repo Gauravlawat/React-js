dfc0528a516254b11615497eedf02b66ad029be3
Gauravlawat <gauravsoni32443@gmail.com>
2025-01-28 12:03:49 +0530

    components block

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,18 @@
-import reactImg from './assets/react-core-concepts.png';
-// import componentImg from './assets/components.png';
-import { CORE_CONCEPTS } from './data.js';
-const reactDescriptions = ['Fundamental', 'Core', 'Crucial'];
-function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
 
-function CoreConcept({image,title,description}) {
-  return (
-    <li>
-      {/* <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p> */}
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
-function Header() {
-  const description = reactDescriptions[getRandomInt(2)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-};
+import { CORE_CONCEPTS } from './data.js';
+import Header from './components//Header/Header.jsx';
+import CoreConcept from './components/Coreconcepts.jsx';
+import TapButton from './components/tapButton.jsx';
+
 
 function App() {
+  function handleSelect() {
+    console.log("hello world - selected");
+  }
+
   return (
     <div>
-      <Header />
-
+      <Header />;
       <main>
         <section id="core-concepts">
           <h2>Core Concepts</h2>
@@ -46,10 +22,20 @@ function App() {
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
-            <CoreConcept {...CORE_CONCEPTS[1]}/>
-            <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TapButton onSelect={handleSelect}>Components</TapButton>
+            <TapButton onSelect={handleSelect}>JSX</TapButton>
+            <TapButton onSelect={handleSelect}>Props</TapButton>
+            <TapButton onSelect={handleSelect}>State</TapButton>
+          </menu>
+
         </section>
       </main>
     </div>
---
+++ src/components/TapButton.jsx
@@ -0,0 +1,11 @@
+export default function TapButton({children}){
+    function handleClick(){
+        console.log("hello world");
+        
+    }
+    return(
+        <li>
+            <button onClick={handleClick}>{children}</button>
+        </li>
+    )
+}
